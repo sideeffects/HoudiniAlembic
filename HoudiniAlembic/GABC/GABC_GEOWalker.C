4fa2f532bdd5c1d2da879f5fd46856b3f2c9ab63
Alex Shelkovnykov <ashelkovnykov@sidefx.com>
2014-07-23 16:24:36 -0400

    Undo commit r206022

--- HoudiniAlembic/GABC/GABC_GEOWalker.C
+++ HoudiniAlembic/GABC/GABC_GEOWalker.C
@@ -662,14 +662,6 @@ namespace {
 	typename		 T::sample_type psample;
 	param.getExpanded(psample, iss);
 
-        // Some attributes (ex. UVs) are point attributes for some shape
-        // types and vertex attributes for others in Alembic. Upgrade all
-        // such attributes to vertex attributes.
-	if(owner == GA_ATTRIB_POINT && gdp.findVertexAttribute(name))
-	{
-	    owner = GA_ATTRIB_VERTEX;
-	}
-
 	switch (owner)
 	{
 	    case GA_ATTRIB_POINT:
