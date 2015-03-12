c6571b191ac4333f0da7425369bf867dffe93deb
Halfdan Ingvarsson <halfdan@sidefx.com>
2015-03-12 13:23:08 -0400

    Finish EXPORT_<lib> to <lib>_EXPORTS sweep.

--- HoudiniAlembic/GABC/GABC_API.h
+++ HoudiniAlembic/GABC/GABC_API.h
@@ -1,45 +1,13 @@
-/*
- * Copyright (c) 2015
- *	Side Effects Software Inc.  All rights reserved.
- *
- * Redistribution and use of Houdini Development Kit samples in source and
- * binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. The name of Side Effects Software may not be used to endorse or
- *    promote products derived from this software without specific prior
- *    written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY SIDE EFFECTS SOFTWARE `AS IS' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
- * NO EVENT SHALL SIDE EFFECTS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *----------------------------------------------------------------------------
- */
 
 #ifndef __GABC_API_h__
 #define __GABC_API_h__
 
 #include <SYS/SYS_Visibility.h>
 
-#ifdef EXPORT_GABC
+#ifdef GABC_EXPORTS
 #define GABC_API SYS_VISIBILITY_EXPORT
-#define GABC_API_TINST SYS_VISIBILITY_EXPORT_TINST
 #else
 #define GABC_API SYS_VISIBILITY_IMPORT
-#define GABC_API_TINST SYS_VISIBILITY_IMPORT_TINST
-#endif
-
-#ifndef GABC_NAMESPACE
-    #define GABC_NAMESPACE	GABC_Houdini
 #endif
 
 #endif
