621434b240cb88bb104660c5a542c627c80fa7db
Derrick Moser <derrick@sidefx.com>
2017-08-11 14:59:09 -0400

    The Alembic ROP now reports errors when it fails to cook a SOP network.

--- src/ROP/ROP_AlembicOut.C
+++ src/ROP/ROP_AlembicOut.C
@@ -1367,7 +1367,10 @@ ROP_AlembicOut::refineSop(
     GU_DetailHandleAutoReadLock rlock(gdh);
     const GU_Detail *gdp = rlock.getGdp();
     if(!gdp)
+    {
+	reportCookErrors(sop, time);
 	return;
+    }
 
     GA_ROHandleS up_vals(gdp, GA_ATTRIB_PRIMITIVE,
 			 GABC_Util::theUserPropsValsAttrib);
@@ -1560,10 +1563,7 @@ ROP_AlembicOut::updateFromSop(
     const GU_Detail *gdp = rlock.getGdp();
     if(!gdp)
     {
-	UT_WorkBuffer buf;
-	sop->getFullPath(buf);
-	myArchive->getOError().error("Error cooking %s at time %g.",
-				     buf.buffer(), time);
+	reportCookErrors(sop, time);
 	return false;
     }
 
@@ -2111,6 +2111,18 @@ ROP_AlembicOut::updateFromHierarchy(
     }
 
     return true;
+}
+
+void
+ROP_AlembicOut::reportCookErrors(OP_Node *node, fpreal time)
+{
+    UT_WorkBuffer buf;
+    node->getFullPath(buf);
+    myArchive->getOError().error("Error cooking %s at time %g.",
+				 buf.buffer(), time);
+    UT_String errors;
+    node->getErrorMessages(errors);
+    addError(ROP_COOK_ERROR, (const char *)errors);
 }
 
 void
