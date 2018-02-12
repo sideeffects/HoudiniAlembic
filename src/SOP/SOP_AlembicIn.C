9151423d1d4e8426235f95aa9d22fadc47b27383
Derrick Moser <derrick@sidefx.com>
2018-02-12 16:05:35 -0500

    Updated order of "File Name" and "Number of Layers" parameters on the Alembic SOP, Alembic Archive Object, and Alembic Xform Object to make their behaviour easier to understand.

--- src/SOP/SOP_AlembicIn.C
+++ src/SOP/SOP_AlembicIn.C
@@ -358,8 +358,8 @@ static PRM_Template prm_AttributeRemapTemplate[] = {
 };
 
 static PRM_Name prm_reloadbutton("reload", "Reload Geometry");
-static PRM_Name prm_filenameName("fileName", "File Name");
 static PRM_Name prm_numlayersName("numlayers", "Number of Layers");
+static PRM_Name prm_filenameName("fileName", "File Name");
 static PRM_Name prm_frameName("frame", "Frame");
 static PRM_Name prm_fpsName("fps", "Frames Per Second");
 static PRM_Name prm_missingFileName("missingfile", "Missing File");
@@ -604,10 +604,10 @@ PRM_Template SOP_AlembicIn2::myTemplateList[] =
 {
     PRM_Template(PRM_CALLBACK, 1, &prm_reloadbutton,
 	    0, 0, 0, SOP_AlembicIn2::reloadGeo),
-    PRM_Template(PRM_FILE,  1, &prm_filenameName, &prm_filenameDefault,
-	    0, 0, 0, &theAbcPattern),
     PRM_Template(PRM_MULTITYPE_LIST, prm_layersTemplate, 1,
             &prm_numlayersName, 0, 0, &PRM_SpareData::multiStartOffsetOne),
+    PRM_Template(PRM_FILE,  1, &prm_filenameName, &prm_filenameDefault,
+	    0, 0, 0, &theAbcPattern),
     PRM_Template(PRM_FLT_J, 1, &prm_frameName, &prm_frameDefault),
     PRM_Template(PRM_FLT_J, 1, &prm_fpsName, &prm_fpsDefault),
     PRM_Template(PRM_ORD,   1, &prm_missingFileName, &missingFileDefault,
