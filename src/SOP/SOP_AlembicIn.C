e69c73f3b0529ed2cf571e53bddd82cfe8de7cde
Derrick Moser <derrick@sidefx.com>
2022-07-08 14:09:12 -0400

    Updated the position of the Alembic SOP's "Reload Geometry" button to be more consistent with other SOP's referencing external files.

--- src/SOP/SOP_AlembicIn.C
+++ src/SOP/SOP_AlembicIn.C
@@ -412,9 +412,9 @@ static PRM_Template prm_AttributeRemapTemplate[] = {
     PRM_Template()
 };
 
-static PRM_Name prm_reloadbutton("reload", "Reload Geometry");
 static PRM_Name prm_numlayersName("numlayers", "Number of Layers");
 static PRM_Name prm_filenameName("fileName", "File Name");
+static PRM_Name prm_reloadbutton("reload", "Reload Geometry");
 static PRM_Name prm_frameName("frame", "Frame");
 static PRM_Name prm_fpsName("fps", "Frames per Second");
 static PRM_Name prm_missingFileName("missingfile", "Missing File");
@@ -665,12 +665,12 @@ sopCreateObjectPathSpareData(bool clear_path, bool clear_exclude)
 
 PRM_Template SOP_AlembicIn::myTemplateList[] =
 {
-    PRM_Template(PRM_CALLBACK, 1, &prm_reloadbutton,
-	    0, 0, 0, SOP_AlembicIn::reloadGeo),
     PRM_Template(PRM_MULTITYPE_LIST, prm_layersTemplate, 1,
             &prm_numlayersName, 0, 0, &PRM_SpareData::multiStartOffsetOne),
     PRM_Template(PRM_FILE,  1, &prm_filenameName, &prm_filenameDefault,
 	    0, 0, 0, &theAbcPattern),
+    PRM_Template(PRM_CALLBACK, 1, &prm_reloadbutton,
+	    0, 0, 0, SOP_AlembicIn::reloadGeo),
     PRM_Template(PRM_FLT_J, 1, &prm_frameName, &prm_frameDefault),
     PRM_Template(PRM_FLT_J, 1, &prm_fpsName, &prm_fpsDefault),
     PRM_Template(PRM_ORD,   1, &prm_missingFileName, &missingFileDefault,
